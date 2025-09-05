2fa59c2b26e6a455756de9c473dda4afc1d068d7
Giordana K. Martins <130246793+giordanamartins@users.noreply.github.com>
2025-09-04 21:58:50 -0300

    add: segunda opção de heuristíca

--- exercicios/maquinas_paralelas.cpp
+++ exercicios/maquinas_paralelas.cpp
@@ -6,7 +6,8 @@ int main(){
     // deixa o código rápido
     ios::sync_with_stdio(0);
     cin.tie(0); 
-    
+
+    // heuristíca 1 - insere as tarefas alternadamente nas máquinas
     ll maq, tt, m = 0;
     cin >> maq >> tt;
     vector<ll> v(tt);  
@@ -19,5 +20,24 @@ int main(){
     }
     cout << m << "\n";
 
+    // ---------------------------------------------------------------------------------
+    // heuristíca 2 - insere as tarefas na máquina com menor tempo total disponível
+    ll menorMaquina;
+    for (int i = 0; i < tt; i++){
+        cin >> v[i]; // vetor armazena o tempo de cada tarefa
+        ll menorTempo = 111111111111111;
+        for (int j = 0; j < tt; j++){
+                if (vmaq[j] < menorTempo){ // se o tempo total de tarefas da máquina for menor que um número grande aleatório
+                    menorTempo = vmaq[j]; // armazena o menor tempo para comparar com as demais nas próximas iterações
+                    menorMaquina = j; // armazena em qual máquina a tarefa será inserida
+                }
+            }
+            vmaq[menorMaquina] += v[i]; // aloca as tarefas nas máquinas disponíveis
+            m = max(m, vmaq[menorMaquina]); // verifica o tempo máximo que uma máquina leva (solução)
+    }
+    cout << m << "\n";
+    // ---------------------------------------------------------------------------------
+
     return 0;
 }
+
